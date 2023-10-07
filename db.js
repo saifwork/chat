42e3c8e007988774d32c8893c090311d0f8fcdca
vishal kumar <vk8953389959@gmail.com>
2023-10-08 01:47:39 +0530

    db conn changes

--- db.js
+++ db.js
@@ -1,16 +1,30 @@
-const mongoose = require('mongoose')
-// const url = 'mongodb://localhost:27017/mycontacts-backend';
+const mongoose = require('mongoose');
 const pwd = process.env.DB_PWD;
-const url = `mongodb+srv://saifwork30:${pwd}@cluster0.vcdnqdm.mongodb.net/`;
+const url = `mongodb+srv://saifwork30:${pwd}@cluster0.vcdnqdm.mongodb.net/test`;
 
 module.exports.connectDb = async () => {
-    mongoose.connect(url, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        family: 4,
-    })
-    const con = mongoose.connection
-    con.on('open', () => {
-        console.log('connected...')
-    })
+    try {
+        await mongoose.connect(url, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            family: 4,
+        });
+
+        const con = mongoose.connection;
+
+        con.on('error', (error) => {
+            console.error('Connection error:', error);
+        });
+
+        con.once('open', () => {
+            console.log('Connected to MongoDB');
+        });
+    } catch (error) {
+        console.error('Error connecting to MongoDB:', error);
+    }
 };
+
+
+
+
+// const url = 'mongodb://localhost:27017/mycontacts-backend';
--- server.js
+++ server.js
@@ -13,17 +13,25 @@ const port = process.env.PORT || 4000;
 
 const app = express();
 const server = http.createServer(app);
-// const io = socketIo(server);
 
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
 
 // Database Connection
+(async () => {
+  try {
+    await db.connectDb();
+    console.log('Connected to MongoDB');
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error);
+    process.exit(1); // Exit the application if the database connection fails
+  }
+})();
 
 // Socket.io Initialization
 initializeSocket(server);
-  
+
 // API Routes
 app.get("/api/test", (req, res) => {
   res.send("HII BUDDY");
@@ -33,12 +41,10 @@ app.use("/api/contacts", require("./routes/contactRoutes"));
 app.use("/api/users", require("./routes/userRoutes"));
 app.use("/api/chat", require("./routes/chatRoutes.js"));
 
-
 // Error Handling Middleware
 app.use(errorHandler);
 
 // Start the Server
 server.listen(port, () => {
   console.log(`Server running on port ${port}`);
-  db.connectDb();
 });
